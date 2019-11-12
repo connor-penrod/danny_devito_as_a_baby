aa0bd6400e151f6f2d5ccec7c733fa428b004489
Connor Penrod <cfpvf9@mail.missouri.edu>
2019-11-12 17:32:47 -0600

    Added better documentation

--- faces.py
+++ faces.py
@@ -9,9 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+#probably useless
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def showImage(img):
+    #show image using matplotlib
+
     print("show")
     arr = np.array(img, dtype=np.uint8)
     plt.figure()
@@ -19,6 +22,8 @@ def showImage(img):
     
     
 def getConfig(name = 'config.txt'):
+    #grabs API key and endpoint data from config.txt file
+
     filename = name
     configs = {}
     try:
@@ -33,6 +38,8 @@ def getConfig(name = 'config.txt'):
         print("'%s' file not found" % filename)
         
 def showPoints(img, face):
+    #this simply illustrates the 27 facial points that Azure Face API returns
+    
     '''
      :param pupil_left:
     :type pupil_left: ~azure.cognitiveservices.vision.face.models.Coordinate
@@ -128,6 +135,9 @@ def showPoints(img, face):
         draw.ellipse([x-radius,y-radius,x+radius,y+radius], fill='red')
 
 def normalizeDevito(image, dev_img, points):
+    #normalizes devito's face to better match baby's skin tone.
+    #UNDER CONSTRUCTION
+
     dev_array = np.array(dev_img, dtype=np.uint8)
     im_array = np.array(image)
     
@@ -153,71 +163,93 @@ def normalizeDevito(image, dev_img, points):
     return dev_img
         
 def placeDevito(image, dev_img, fc):
+    #simply pastes the devito image over the baby's face
+
+    #get baby's face coordinates
     points = fc.face_rectangle
    
-    dev_img = normalizeDevito(image, dev_img, points)
+    #optionally normalize devito's face first
+    #dev_img = normalizeDevito(image, dev_img, points)
     
+    #resize devito image to baby's face size
     dev_img = dev_img.resize((points.width, points.height))
+    
+    #paste devito over baby's face
     image.paste(dev_img, box=(points.left,points.top,points.left+points.width,points.top+points.height), mask=dev_img) 
 
 def cloneDevito(image, dev_img, fc):
+    #this function runs poisson image cloning on devito and the baby's face
+    
+    #get bounding box around baby's face
     points = fc.face_rectangle
+    
+    #convert PIL image to numpy array
     image = np.array(image, dtype=np.uint8)
+    
+    #save alpha channel for later
     dev_alpha = dev_img
+    
+    #convert devito image to numpy array, discard alpha channel, and resize to fit baby's face
     dev_img = np.array(dev_img, dtype=np.uint8)
     dev_img = dev_img[:,:,:3]
     dev_img = cv2.resize(dev_img, dsize=(points.width, points.height), interpolation=cv2.INTER_NEAREST)#dev_img = dev_img.resize((points.width, points.height))
     
     
-    
+    #create source mask that is the size of the baby's face
     src_mask = 255 * np.ones(dev_img.shape, dev_img.dtype)
-    print(image.shape)
-    print(dev_img.shape)
-    print((int(points.left + (points.width/2)),int(points.top + (points.height/2))))
+    
+    #run cv2.seamlessClone using the two images and the source mask
     image = cv2.seamlessClone(dev_img, image, src_mask, (int(points.left + (points.width/2)),int(points.top + (points.height/2))), cv2.NORMAL_CLONE)
     return image
     
 def processImage(url):
+    #processImage handles all processing on each face in a given URL
+
+    #load in family photo
     single_face_image_url = url
     response = requests.get(single_face_image_url)
     img = Image.open(io.BytesIO(response.content))
 
+    #load in Danny Devito image
     devito_file = "d_0.png"
-    #response2 = requests.get(devito_url)
     devito_img = Image.open(devito_file)
     
-    
+    #create a FaceClient object using Azure credentials
     face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
+    #collect face information using Azure
     detected_faces = face_client.face.detect_with_url(url=single_face_image_url, return_face_landmarks=True, return_face_attributes=['age','gender'])
     if not detected_faces:
         print("No faces, baby or otherwise.")
         return
 
-    draw = ImageDraw.Draw(img)
-    minFace = detected_faces[0]
     for face in detected_faces:
         if float(face.face_attributes.age) < BABY_THRESHOLD:
+            #perform processing on each individual baby's face
             showPoints(img, face)
             #img = cloneDevito(img, devito_img, face)
     
-    #points = minFace.face_rectangle
-    #devito_img = devito_img.resize((points.width, points.height))
-    #img.paste(devito_img, box=(points.left,points.top,points.left+points.width,points.top+points.height), mask=None)   
-    print("about to show")
     showImage(img)
     
 if __name__ == "__main__":
 
+    #grab API key and endpoint from config.txt file
+    #example config.txt:
+    #   key = 1234
+    #   endpoint = www.endpoint.com 
     configs = getConfig()
     KEY = configs['key']
     ENDPOINT = configs['endpoint']
+    
+    #this specifies minimum estimated age for a face to be considered a baby's face
     BABY_THRESHOLD = 10
     
+    #grab urls from command line arguments
     urls = sys.argv[1:]
     
     for url in urls:
-        print(url)
         processImage(url)
+        
+    #show edited image at the end
     plt.show()
     
