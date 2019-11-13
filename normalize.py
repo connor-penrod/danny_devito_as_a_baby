0d6e6709bb0fee6953dc6e7a0eeece155e15c58e
Connor Penrod <cfpvf9@mail.missouri.edu>
2019-11-13 13:44:32 -0600

    Added histogram normalization

--- normalize.py
+++ normalize.py
@@ -1,34 +1,78 @@
-from io import BytesIO
-from PIL import Image, ImageDraw
 import numpy as np
 
-def normalization(im1,im2):
-    #im1.show()
-    im1 = np.array(im1)
-    im1_min = np.min(im1, axis=(0,1))
-    im1_max = np.max(im1, axis=(0,1))
+def hist_match(source, template):
+    """
+    Author: ali_m @ StackOverflow
+    
+    Adjust the pixel values of a grayscale image such that its histogram
+    matches that of a target image
 
-    im2 = np.array(im2)
-    im2_min = np.min(im2, axis=(0, 1))
-    im2_max = np.max(im2, axis=(0, 1))
+    Arguments:
+    -----------
+        source: np.ndarray
+            Image to transform; the histogram is computed over the flattened
+            array
+        template: np.ndarray
+            Template image; can have different dimensions to source
+    Returns:
+    -----------
+        matched: np.ndarray
+            The transformed output image
+    """
 
-    comb_min = (im1_min + im2_min)/2
-    comb_max = (im1_max + im2_max)/2
-    comb_avg = (comb_min+comb_max)/2
-    print(comb_avg)
-    
-    im1 = im1/comb_avg * 50
-    #print(im1)
-    im2 = im2/comb_avg * 50
-    
-    im1_pil = Image.fromarray(im1.astype('uint8'), 'RGB')
-    im2_pil = Image.fromarray(im2.astype('uint8'), 'RGB')    
-    im1_pil.show()
-    im2_pil.show()
-    
-    
-    
-image1 = Image.open("devito1.png")
-image2 = Image.open("family1.jpg")
+    oldshape = source.shape
+    source = source.ravel()
+    template = template.ravel()
 
-normalization(image1,image2)
+    # get the set of unique pixel values and their corresponding indices and
+    # counts
+    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                            return_counts=True)
+    t_values, t_counts = np.unique(template, return_counts=True)
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    return interp_t_values[bin_idx].reshape(oldshape)    
+    
+def normalize(source, template):
+    match_r = hist_match(source[:,:,0], template[:,:,0])
+    match_g = hist_match(source[:,:,1], template[:,:,1])
+    match_b = hist_match(source[:,:,2], template[:,:,2])
+
+    match = np.dstack((match_r,match_g,match_b))
+    match = match.astype(np.uint8)
+    return match
+    
+if "__name__" == "__main__":
+    from io import BytesIO
+    from PIL import Image, ImageDraw
+    import matplotlib.pyplot as plt
+    image1 = Image.open("devito1.png")
+    image2 = Image.open("family1.jpg")
+
+    image1 = np.array(image1)
+    image2 = np.array(image2)
+
+    match = normalize(image1,image2)
+
+    plt.figure()
+    plt.imshow(image1)
+
+    plt.figure()
+    plt.imshow(image2)
+
+    print(match)
+    plt.figure()
+    plt.imshow(match)
+
+plt.show()
